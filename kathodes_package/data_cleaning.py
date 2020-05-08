cd8cae0ed99fbea3b84c5b8b19355d762b5d7f6d
katherine-soderberg <katherine.soderberg@yale.edu>
2020-05-08 11:33:02 -0400

    update data cleaning and outlier assessment modules and demonstrate them in the jupyter notebook

--- kathodes_package/data_cleaning.py
+++ kathodes_package/data_cleaning.py
@@ -1,26 +1,29 @@
 
 def removeMissing(filename):
     """Takes a file that contains missing scans and removes those rows, while providing the subject name and reason for removal."""
+    import pandas as pd
+    import math
     loaded_file = pd.read_csv(filename)
     cleaned_list = []
     missing_counter = 0
     for row in loaded_file.index:
-        if loaded_file[3][row] == "":
-            print("Dropping subject scan " + loaded_file[0][row] + " because of " + loaded_file[1][row])
-            counter = counter + 1
+        if math.isnan(loaded_file.iloc[row, 3]):
+            print("Dropping subject scan " + loaded_file.iloc[row, 0] + " because of " + loaded_file.iloc[row,1])
+            missing_counter = missing_counter + 1
         else:
-            cleaned_list.append(loaded_file[:][row])
-    print("There were " + str(counter) + " scans with missing data dropped.")
+            cleaned_list.append(loaded_file.iloc[row])
+    print("There were " + str(missing_counter) + " scans with missing data dropped.")
     cleaned_df = pd.DataFrame(cleaned_list)
     return cleaned_df
 
 def voxelConsistency(cleaned_dataframe, column_number, expected_size):
     """Checks that every scan has the same voxel dimension, specified by the user."""
+    import pandas as pd
     consistency_boolean = True
-    for row in cleaned_dataframe.index:
-        if cleaned_dataframe[column_number][row] == expected_size:
-            continue
-        elif cleaned_dataframe[column_number][row] != expected_size:
-            print("Subject scan " + cleaned_dataframe[0][row] + " does not have voxel size of " +str(expected_size))
+    for row in range(cleaned_dataframe.index.size - 1):
+        if cleaned_dataframe.iloc[row, column_number] != expected_size:
+            print("Subject scan " + cleaned_dataframe.iloc[row, 0] + " with row index of " + str(row) + " does not have voxel size of " + str(expected_size))
             consistency_boolean = False
+        else:
+            continue
     return consistency_boolean
--- kathodes_package/outlier_assessment.py
+++ kathodes_package/outlier_assessment.py
@@ -1,9 +1,10 @@
 
 def outlierStats(outlier_list):
     """Takes a list of outliers and computes the mean and standard deviation"""
+    import statistics
     try:
-        outlierMean = outlier_list.mean()
-        outlierStdev = outlier_list.std()
+        outlierMean = statistics.mean(outlier_list)
+        outlierStdev = statistics.stdev(outlier_list)
         return outlierMean, outlierStdev
     except TypeError :
         explanation = "Cannot compute statistics on a list of non-numerical elements."
@@ -11,15 +12,17 @@ def outlierStats(outlier_list):
     
 def outlierExclude(cleaned_dataframe, column_number, stdev_cutoff_factor):
     """Uses outlierStats to determine which scans have outlying volumes above a specified threshold and removes them."""
-    column_as_series = cleaned_dataframe[column_number]
+    import pandas as pd
+    column_as_series = cleaned_dataframe.iloc[:,column_number]
     column_as_list = column_as_series.tolist()
     mean, stdev = outlierStats(column_as_list)
     upper_threshold = mean + (stdev * stdev_cutoff_factor)
     lower_threshold = mean - (stdev * stdev_cutoff_factor)
     noOutlier_list = []
-    for row in cleaned_dataframe.index:
-        if cleaned_dataframe[column_number][row] > upper_threshold | cleaned_dataframe[column_number][row] < lower_threshold:
-            print("Dropping subject scan " + cleaned_dataframe[0][row] " due to " + str(cleaned_dataframe[column_number][row]) + " outlying volumes." )
+    for row in range(cleaned_dataframe.index.size - 1):
+        if (cleaned_dataframe.iloc[row, column_number] > upper_threshold) or (cleaned_dataframe.iloc[row, column_number] < lower_threshold):
+            print("Dropping subject scan " + cleaned_dataframe.iloc[row, 0] + " due to " + str(cleaned_dataframe.iloc[row, column_number]) + " outlying volumes." )
         else:
-            noOutlier_list.append(cleaned_dataframe[:][row])
+            noOutlier_list.append(cleaned_dataframe.iloc[row])
     noOutlier_dataframe = pd.DataFrame(noOutlier_list)
+    return noOutlier_dataframe
